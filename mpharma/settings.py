6ea59d18e76f816c212e3d62e424763d56bc247e
Ebo Turkson <mturkson23@gmail.com>
2021-01-29 04:24:54 +0000

    add testing definitions in settings

--- mpharma/settings.py
+++ mpharma/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'), 
+        'TEST': {
+            'NAME': 'dbicd10',
+        }
     }
 }
 
